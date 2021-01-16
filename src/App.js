23550c89c8036d4126608eec597b704e2d4c8ce2
Hamida Sh <sharifovahamida@gmail.com>
2021-01-16 14:34:29 -0800

    blank project

--- src/App.js
+++ src/App.js
@@ -1,104 +1,18 @@
 import React, { Component } from "react";
-import TodoList from "./components/TodoList";
-import TodoForm from "./components/TodoForm";
-import "./styles.css";
 
-//1. Add dummy tasks data
-
-const tasks = [
-  {
-    taskName: "Go to school",
-    id: 100,
-    completed: false,
-  },
-  {
-    taskName: "Learn JS",
-    id: 200,
-    completed: false,
-  },
-];
-
-//2. Build App component as a class
+const tasks = [];
 
 class App extends Component {
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
 
-  // 3. Add state to our app, which takes dummy data from the above
-  constructor() {
-    super();
-    this.state = {
-      tasks: tasks,
-    };
-  }
 
-  /*5. addTask function to handle new task creation once form is submitted and update the App state (this.state.tasks)
-        by using this.setState function */
-
-  addTask = (taskName) => {
-    const newTask = {
-      tasks: [
-        ...this.state.tasks,
-        {
-          id: Date.now(),
-          taskName: taskName,
-          completed: false,
-        },
-      ],
-    };
-
-    this.setState(newTask);
-  };
-
- /*6. completeTask function to handle the click on any "Task" div, 
- and match the task id passed in its arguments and change its "completed" status to opposite value, */
-
-  completeTask = (taskId) => {
-    this.setState({
-      tasks: this.state.tasks.map((task) => {
-        if (task.id == taskId) {
-          return {
-            ...task,
-            completed: !task.completed,
-          };
-        }
-        return task;
-      }),
-    });
-  };
-
-  /*7. clearDoneTasks to handle Clear button on the TodoForm and filter Tasks data by task completed status is false (not completed) 
-  and use setState function to set Tasks data to newly flitered array where only tasks with task completed status of false are left*/ 
-
-  clearDoneTasks = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.filter((task) => {
-        return task.completed === false; 
-      }),
-    });
-  };
-
-
-  // 4. Render Todo Form component and TodoList component as separate sections within the App
   render() {
     return (
       <div className="App">
-        <section className="form">
-          <TodoForm
-            tasks={this.state.tasks}
-            addTask={this.addTask}
-            clearDoneTasks={this.clearDoneTasks}
-          />
-        </section>
-        <section className="tasks">
-          <TodoList
-            tasks={this.state.tasks}
-            clearDoneTasks={this.clearDoneTasks}
-            completeTask={this.completeTask}
-          />
-        </section>
+        
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,27 +1,2 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
-import React from 'react';
-import Task from './Todo'
-
-/*1. Function to create a Div to hold all tasks in it.
-      - Map over tasks data (which is passed from App.js as a prop) and create a new "Task" for each item in the tasks data
-      - Task component gets the id of each task(item from Tasks data) as its key
-      - Task component also gets 2 props from TodoList.js: 
-            -each task object (from our map function) 
-            -completeTask function which is passed from App.js as props
-*/
-function TodoList(props) {
-    return(
-        <div className="tasks">
-            {props.tasks.map(task => {
-                return(
-                    <Task key = {task.id} task = {task}  completeTask = {props.completeTask}/>
-                )
-            })}
-            
-
-        </div>
-    )
-}
-
-export default TodoList;
